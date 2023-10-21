5fb5cbd4d6cccdee9e04ebb8f722ed97779f4d40
Daleock1 <142682523+Daleock1@users.noreply.github.com>
2023-10-21 11:49:17 -0500

    "Cliente y EventoBancario" Benjamin Madrid Jara

---
+++ EventoBancario.java
@@ -0,0 +1,41 @@
+package banco;
+
+import java.util.Date;
+
+public class EventoBancario {
+
+	Date fecha;
+	String tituloEvento;
+	String descripcionEvento;
+	public EventoBancario(Date fecha, String tituloEvento, String descripcionEvento) {
+		super();
+		this.fecha = fecha;
+		this.tituloEvento = tituloEvento;
+		this.descripcionEvento = descripcionEvento;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getTituloEvento() {
+		return tituloEvento;
+	}
+	public void setTituloEvento(String tituloEvento) {
+		this.tituloEvento = tituloEvento;
+	}
+	public String getDescripcionEvento() {
+		return descripcionEvento;
+	}
+	public void setDescripcionEvento(String descripcionEvento) {
+		this.descripcionEvento = descripcionEvento;
+	}
+	@Override
+	public String toString() {
+		return "EventoBancario [fecha=" + fecha + ", tituloEvento=" + tituloEvento + ", descripcionEvento="
+				+ descripcionEvento + "]";
+	}
+	
+
+}
