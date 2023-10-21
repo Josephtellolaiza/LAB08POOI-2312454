fb2f550439a6c2fe1744679f40c0bbe53d1c7a35
Josephtellolaiza <142683026+Josephtellolaiza@users.noreply.github.com>
2023-10-21 12:06:28 -0500

    Add files via upload
    Adicion de las clases hijas

--- Banco/src/banco/CuentaCTS.java
+++ Banco/src/banco/CuentaCTS.java
@@ -1,10 +1,29 @@
 package banco;
 
 public class CuentaCTS {
+	    private double tasalnteresAnual = 0.07; 
+	    private double puntos;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    public CuentaCTS(double puntos) {
+	        this.puntos = puntos;
+	    }
 
+	    public double getPuntos() {
+	        return puntos;
+	    }
+
+	    public void setPuntos(double puntos) {
+	        this.puntos = puntos;
+	    }
+
+	    public double calcularIntereses() {
+	        return puntos * tasalnteresAnual;
+	    }
+	    public String toString() {
+	        return "Cuenta CTS - Puntos: " + puntos + ", Intereses acumulados: " + calcularIntereses();
+	    }
 	}
 
-}
+	
+
+
